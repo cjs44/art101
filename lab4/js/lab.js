4be55594567b27454d8bab27180e586f06620f69
cjs44 <129994072+cjs44@users.noreply.github.com>
2023-04-24 12:19:20 -0700

    lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,24 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Pac-Man
+// Pseudocode of the game
+// Caitlin Smith <csmith44@ucsc.edu>
+// Alissa Sheinin <asheinin@ucsc.edu>
+// April 2023
 
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// Print an empty maze
+// Set lives and scores to 0
+    // Print this text on the screen
+// Loop that runs while he is still alive
+    // Print the score and lives as text at the top
+    // Print the current screen
+    // Create the Pac-Man character that will be moved by the user
+      // Take user input to move Pac-Man using coordinates
+    // Create the ghosts that will move automatically
+      // Algorithm to move them randomly (random coordinates)
+      // When Pac-Man touches them, he loses a life
+    // Print the dots in all paths that will be eaten by Pac-Man
+      // They disappear as Pac-Man passes over them
+      // Increase score 
+    // Print the bigger powerup dots
+      // They disappear as Pac-Man passes over them
+      // He does not lose a life the next time he touches a ghost
+// Print game over message
