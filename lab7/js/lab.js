095471ec993fb3f8c410db7c1a25d3408fd2913e
cjs44 <129994072+cjs44@users.noreply.github.com>
2023-05-05 10:28:19 -0700

    lab7

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -1,21 +1,22 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 7 Functions
+// Author: Caitlin Smith and Alice Sheinin
+// Date: 5/3/23
 
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+//a new function - userNameUCSC
+function userNameUCSC() {
+  var userName = window.prompt("Please provide your username");
+  console.log("userName =", userName);
+  //split string to array
+  var nameArray = userName.split('');
+  console.log("nameArray =", nameArray);
+  //sort the array
+  var nameArraySort = nameArray.sort();
+  console.log("nameSort =", nameArraySort);
+  //join array back to string
+  var nameSorted = nameArraySort.join('');
+  console.log("nameSorted =", nameSorted);
+  return nameSorted;
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+//call the function and output
+document.writeln("Thank you, I found your name:  ", userNameUCSC(), "</br");
