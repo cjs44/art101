ff68d3507fc0f02ba4a4d5e5c2f5d41497735b3c
cjs44 <129994072+cjs44@users.noreply.github.com>
2023-05-17 23:28:33 -0700

    .

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -1,21 +1,22 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 11 Libraries & jQuery
+// Author: Caitlin Smith and Alice Sheinin
+// Date: 5/17/23
 
-// Constants
 
-// Functions
+//create buttons with text labels and add to sections. 
+$("#challenge").append("<button id='c'>Challenge</button>");
+$("#problem").append("<button id='p'>Problem</button>");
+$("#result").append("<button id='r'>Result</button>");
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+//click event to the challenge buttom and toggle 
+$("#c").click(function(){
+    $(this).parent().toggleClass("special");
+})
+//click event to the problem buttom and toggle 
+$("#p").click(function(){
+    $(this).parent().toggleClass("special");
+})
+//click event to the result buttom and toggle 
+$("#r").click(function(){
+    $(this).parent().toggleClass("special");
+})
