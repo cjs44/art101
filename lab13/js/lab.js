27aa2ac0f6de8821ff1b2ca7152b175991ac45ff
cjs44 <129994072+cjs44@users.noreply.github.com>
2023-05-24 18:16:39 -0700

    13

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -7,12 +7,6 @@ function FizzBuzz(str) {
   for (var i = 1; i <= 200; i++){
     oneLongString += i + " ";
     var flag = 0;
-    /*If the number is a multiple of 3, if should print "Fizz!"
-If the number is a multiple of 5, it should print "Buzz!"
-If the number is a multiple of 7, it should print "Boom!"
-If the number is a multiple of more than one 3, 5 or 7, it should combine Fizz, 
-Buzz, and Boom. For example, if the number is a multiple of 3 AND 5, it should print 
-"FizzBuzz!" and so on. */
     if (i % 3 == 0){
       oneLongString += "Fizz";
       flag = 1;
